b382331333a391cec67e2bff73c8b6b6007b169b
Piyush dantani <Piyush.dantani@HITECHENG.LOCAL>
2024-10-16 15:42:43 +0530

    16 10 2024

---
+++ src/OCT/ex_151024/Lab001.java
@@ -0,0 +1,9 @@
+package OCT.ex_151024;
+
+import java.net.StandardSocketOptions;
+
+public class Lab001 {
+    public static void main(String[] args) {
+        System.out.println("First Test");
+    }
+}
---
+++ src/OCT/ex_161024_Main_method/Lab002.java
@@ -0,0 +1,7 @@
+package OCT.ex_161024_Main_method;
+
+public class Lab002 {
+    public static void main(String[] args) {
+
+    }
+}
